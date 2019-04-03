bba2f0e5e7d4ffe6de2e5db58b3b96165a36ad84
Heungsoo Kim <38024642+harusoo@users.noreply.github.com>
2019-04-02 21:12:58 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 var nav = document.getElementByClassName("navigation");
 var main = document.getElementById("main");
 var menu = document.getElementsByClassName("menuitems");
-var close = document.getElementByClass("closebtn");
+var close = document.getElementsByClass("closebtn");
 
 //default to measure if/else from
 nav.style.height = "50px";
