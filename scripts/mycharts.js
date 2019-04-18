33a22002c4965012f3bbdac4169a6b58489e767b
Heungsoo Kim <38024642+harusoo@users.noreply.github.com>
2019-04-17 21:52:48 -0500

    Upload mycharts.js

---
+++ scripts/mycharts.js
@@ -0,0 +1,103 @@
+var ctx = document.getElementById('myChart1').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Open to networking', 'Not open to networking'],
+        datasets: [{
+            label: '# of Votes',
+            data: [72.2, 27.8],
+            backgroundColor: [
+                'rgba(135, 131, 209, 0.8)',
+                'rgba(178, 226, 242, 0.8)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 0)',
+                'rgba(54, 162, 235, 0)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+      legend: {
+        display: false,
+      }
+    }
+});
+
+var ctx = document.getElementById('myChart2').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Real-time collaboration useful', 'Real-time collaboration not useful'],
+        datasets: [{
+            label: '# of Votes',
+            data: [72.2, 22.8],
+            backgroundColor: [
+                'rgba(135, 131, 209, 0.8)',
+                'rgba(178, 226, 242, 0.8)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 0)',
+                'rgba(54, 162, 235, 0)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+      legend: {
+        display: false,
+      }
+    }
+});
+
+var ctx = document.getElementById('myChart3').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Uses cloud storage', 'Does not use cloud storage'],
+        datasets: [{
+            label: '# of Votes',
+            data: [72.2, 22.8],
+            backgroundColor: [
+                'rgba(135, 131, 209, 0.8)',
+                'rgba(178, 226, 242, 0.8)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 0)',
+                'rgba(54, 162, 235, 0)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+      legend: {
+        display: false,
+      }
+    }
+});
+
+var ctx = document.getElementById('myChart4').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Necessary for work', 'Unnecessary for work'],
+        datasets: [{
+            label: '# of Votes',
+            data: [72.2, 22.8],
+            backgroundColor: [
+                'rgba(135, 131, 209, 0.8)',
+                'rgba(178, 226, 242, 0.8)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 0)',
+                'rgba(54, 162, 235, 0)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+      legend: {
+        display: false,
+      }
+    }
+});
